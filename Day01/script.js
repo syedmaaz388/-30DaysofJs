16a048d6070ea9240369af49913ff3816f55ccce
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2024-04-12 21:17:51 +0530

    Add files via upload

---
+++ Day01/script.js
@@ -0,0 +1,52 @@
+// Day 01
+
+console.log("Hello");
+console.log("I Like Pizza");
+
+window.alert("Hello");
+alert("Hi");
+
+document.getElementById("myHi").textContent = "Hello Fraands Chai Pe Lo";
+
+// This is Commnet
+
+/*
+Multi
+line
+comment
+*/
+
+// Variable
+let x;
+// Number
+x = 100;
+console.log(x);
+
+// Template Litera;
+console.log(`Nothing is ${x} percent`);
+
+// String
+let Name = "BroCode";
+console.log(Name);
+
+// Boolean
+let online = true;
+console.log(online);
+
+// Type of
+let FullName = "Syed Maaz"
+console.log(typeof FullName)
+
+
+document.getElementById("p1").textContent = `My Name is ${FullName}`;
+let age = 24;
+
+document.getElementById("p2").textContent = `I am ${age} years old`;
+
+let Profession = "Programmer"
+
+document.getElementById("p3").textContent = `I am a ${Profession}`;
+
+
+// Day One Completed
+
