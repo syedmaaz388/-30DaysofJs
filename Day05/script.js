bc4785e9139b31f9eba03705221ae6d6a37bf834
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2024-04-17 00:10:13 +0530

    Update script.js

--- Day05/script.js
+++ Day05/script.js
@@ -23,3 +23,40 @@ console.log(secondchar2);
 const fullNameOne = "Broseph Code";
 let firstNameTwo = fullNameOne.slice(0,fullNameOne.indexOf(" "));
 console.log(firstNameTwo);
+
+
+const email = "Bro@gmail.com";
+let useremail = email.slice(0,email.indexOf("@"));
+console.log(useremail)
+
+let extension = email.slice(email.indexOf("@"));
+console.log(extension)
+
+// Method Chaining
+
+let userKaNaam = "    Brocode";
+console.log(userKaNaam.trim().charAt(0).toLowerCase())
+
+// Logical Operators
+
+
+// And - &&
+// Or - ||
+// Not - !
+
+
+const temp = 20;
+if(temp > 0 && temp <= 30){
+    console.log("The weather is good");
+}else{
+    console.log("Bad Weather");
+}
+
+
+// Strict Eqaulity
+
+// = - Assignment Operator
+// == - Comparision Operator
+// === - Strict eqality operator
+// != - inequality operator
+// !=== - strict inequality operator
