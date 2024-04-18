8d272a91f8f9090a0368b7acc3e8ab4a2bfa92d7
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2024-04-19 00:03:38 +0530

    Update script.js

--- Day07/script.js
+++ Day07/script.js
@@ -1,4 +1,14 @@
 
+let globalVar = 10;
+console.log(globalVar);
+function Hello(){
+    let localVar = 20;
+    console.log(localVar);
+    console.log(globalVar);
+}
+Hello()
+
+// console.log(localVar) 
 
 
 
@@ -26,4 +36,45 @@ temp = value * 9/5 + 32
 
     console.log(value)
     
+}
+
+
+
+
+// Arrays
+
+let fruits = ["Apple","Orange","Banana"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+fruits.push("Guava");
+fruits.push("Cherry");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+fruits.unshift("Mango");
+fruits.unshift("PineApple");
+console.log(fruits)
+fruits.shift();
+console.log(fruits)
+
+console.log(fruits.length)
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+    
+}
+
+for(let i = fruits.length - 1;i >= 0; i--){
+    console.log(fruits[i]);
+}
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+for(let fruit in fruits){
+    console.log(fruit);
 }
